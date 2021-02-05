406b07b6b7f61151bf951d6613be513337cc134f
detols4454 <78596477+detols4454@users.noreply.github.com>
2021-02-05 23:39:12 +0900

    Add files via upload

---
+++ 질문등대야.py
@@ -0,0 +1,13 @@
+import discord
+token = 'Nzk3NzAyNjM1MzM5MjUxNzYy.X_qUeA.FUjHLR9Ts371hAPbfUdS_mFk0zU'
+client = discord.Client()
+@client.event
+async def on_ready():
+    print('로그인되었습니다!')
+    print(client.user.name)
+    print(client.user.id)
+    game = discord.Game('『당신의 오늘은 어떤가요?』질문')
+    await client.change_presence(status=discord.Status.online, activity=game)
+    print('====================================')
+
+client.run(token)
